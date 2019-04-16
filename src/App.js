40288a48de5b92bb944a153f69e537e042ca8c83
yuezihao <yuezihao@joinquant.com>
2019-04-17 02:11:47 +0800

    update axios json server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import 'normalize.css';
+import axios from 'axios'; 
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notebooks: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3100/notebooks').then(res => {
+      console.log(res.data);
+      this.setState({ notebooks: res.data });
+    });
+  }
+
   render() {
+    const notebooks = this.state.notebooks;
     return (
       <div className="app">
         <div className="sidebar">
@@ -21,35 +37,18 @@ class App extends Component {
               </div>
               <div className="body">
                 <ul className="notebooks-list">
-                  <li className="notebook-item">
-                    <div className="title has-icon">
-                      <i className="iconfont icon-book"></i>
-                      默认笔记本
-                    </div>
-                    <button className="button trash"><i className="iconfont icon-trash"></i></button>
-                  </li>
-                  <li className="notebook-item active">
-                    <div className="title has-icon">
-                      <i className="iconfont icon-book"></i>
-                      读书笔记
-                    </div>
-                    <button className="button trash"><i className="iconfont icon-trash"></i></button>
-                  </li>
-                  <li className="notebook-item">
-                    <div className="title has-icon">
-                      <i className="iconfont icon-book"></i>
-                      吃喝玩乐
-                    </div>
-                    <button className="button trash"><i className="iconfont icon-trash"></i></button>
-                  </li>
-                  <li className="notebook-item">
-                    <div className="title has-icon">
-                      <i className="iconfont icon-book"></i>
-                      我的2019
-                    </div>
-                    <button className="button trash"><i className="iconfont icon-trash"></i></button>
-                  </li>
-              </ul>
+                  {
+                    notebooks.map((notebook, index) => (
+                      <li key={notebook.id} className="notebook-item">
+                        <div className="title has-icon">
+                          <i className="iconfont icon-book"></i>
+                          {notebook.name}
+                        </div>
+                        <button className="button trash"><i className="iconfont icon-trash"></i></button>
+                      </li>
+                    ))
+                  }
+                </ul>
               </div>
             </div>
           </div>
@@ -100,7 +99,7 @@ class App extends Component {
               读书笔记
             </div>
             <div className="title">
-              <input type="text" value="读《深入理解ES6》" />
+              <input type="text" />
             </div>
           </div>
           <div className="body">
