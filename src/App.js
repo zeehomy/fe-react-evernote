568307c7f2e9f277529cd64d283408dfe6a3b1c5
yuezihao <yuezihao@joinquant.com>
2019-04-22 00:53:18 +0800

    update evernote

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import 'normalize.css';
+import 'github-markdown-css';
 import axios from 'axios';
-import classNames from 'classnames';
+import cx from 'classnames';
+import marked from 'marked';
 import './App.scss';
 
 class App extends Component {
@@ -10,7 +12,8 @@ class App extends Component {
     this.state = {
       notebooks: [],
       currentBookIndex: 0,
-      notes: []
+      notes: [],
+      currentNote: null
     };
   }
 
@@ -27,11 +30,12 @@ class App extends Component {
 
   render() {
     const notebooks = this.state.notebooks;
+    const currentNote = this.state.currentNote;
     return (
       <div className="app">
         <div className="sidebar">
           <div className="header">
-            <button className="button adder">
+            <button className="button adder" onClick={() => this.handleAddNote()}>
               <i className="iconfont icon-add"></i>
               新建笔记
             </button>
@@ -69,8 +73,8 @@ class App extends Component {
             {
               this.state.notes.map((note, index) => (
                 <li key={note.id}>
-                  <div className="note-brief">
-                    <div className="box">
+                  <div className={cx('note-brief', { active: currentNote && currentNote.id === note.id })}>
+                    <div className="box" onClick={() => this.handleEditNote(note.id)}>
                       <div className="header">{note.title}</div>
                       <div className="body">
                         {note.body}
@@ -78,7 +82,7 @@ class App extends Component {
                     </div>
                     <div className="footer">
                       <div className="datetime">{note.datetime}</div>
-                      <button className="trash button">
+                      <button className="trash button" onClick={() => this.handleDeleteNote(note.id)}>
                         <i className="iconfont icon-trash"></i>
                       </button>
                     </div>
@@ -89,31 +93,37 @@ class App extends Component {
             </ul>
           </div>
         </div>
-        <div className="note-panel">
-          <div className="header">
-            <div className="category has-icon">
-              <i className="iconfont icon-notebook"></i>
-              读书笔记
+        { currentNote ?
+          <div className="note-panel">
+            <div className="header">
+              <div className="category has-icon">
+                <i className="iconfont icon-notebook"></i>
+                读书笔记
+              </div>
+              <div className="title">
+                <input name="title" type="text" value={currentNote.title} onChange={e => this.handleFieldChange(e)}/>
+              </div>
             </div>
-            <div className="title">
-              <input type="text" />
+            <div className="body">
+              <div className="editor">
+                <textarea name="body" 
+                  value={currentNote.body} 
+                  onChange={e => this.handleFieldChange(e)}
+                  onKeyDown={e => this.handlePressTab(e)}
+                ></textarea>
+              </div>
+              <div className="preview markdown-body">
+                <div dangerouslySetInnerHTML={{ __html: marked(currentNote.body) }}></div>
+              </div>
             </div>
-          </div>
-          <div className="body">
-            <div className="editor">
-              <textarea></textarea>
-            </div>
-            <div className="preview markdown-body">
-              <div></div>
-            </div>
-          </div>
-        </div>
+          </div> : null
+        }
       </div>
     );
   }
 
   handleActiveClassName(feildName, defaultClasses, index) {
-    return classNames(...defaultClasses, { active: this.state[feildName] === index });
+    return cx(...defaultClasses, { active: this.state[feildName] === index });
   }
 
   handleSelectBook(index) {
@@ -123,10 +133,80 @@ class App extends Component {
   }
 
   loadNotes(bookId) {
-    axios.get('http://localhost:3100/notes?bookId=' + bookId).then(res => {
+    axios.get(`http://localhost:3100/notes?bookId=${bookId}`).then(res => {
       console.log(res.data);
       this.setState({ notes: res.data });
     });
+  }
+
+  handleAddNote() {
+    const note = {
+      title: '新建笔记',
+      body: '',
+      datetime: new Date().toISOString(),
+      bookId: this.state.notebooks[this.state.currentBookIndex].id
+    };
+    axios.post(`http://localhost:3100/notes${note}`).then(res => {
+      this.reloadNotes();
+    });
+  }
+
+  reloadNotes() {
+    const bookId = this.state.notebooks[this.state.currentBookIndex].id;
+    this.loadNotes(bookId);
+  }
+
+  handleDeleteNote(noteId) {
+    axios.delete(`http://localhost:3100/notes/${noteId}`).then(res => {
+      this.reloadNotes();
+    });
+  }
+
+  handleEditNote(noteId) {
+    axios.get(`http://localhost:3100/notes/${noteId}`).then(res => {
+      this.setState({ currentNote: res.data });
+    });
+  }
+
+  handleFieldChange(e) {
+    const note = {
+      ...this.state.currentNote,
+      [e.target.name]: e.target.value
+    };
+
+    this.setState({
+      currentNote: note
+    });
+    
+    axios.put(`http://localhost:3100/notes/${this.state.currentNote.id}`, note);
+
+    const notes = [...this.state.notes];
+    const index = notes.findIndex(o => o.id === this.state.currentNote.id);
+    if (index !== -1) {
+      notes[index] = note;
+      this.setState({ notes: notes });
+    }
+  }
+
+  handlePressTab(e) {
+    if ( e.keyCode === 9 ) {
+
+      // Set up some variables. We need to know the current position of the cursor or selection.
+      var selectionStartPos = e.target.selectionStart;
+      var selectionEndPos   = e.target.selectionEnd;
+      var oldContent        = e.target.value;
+
+      // Set the new content.
+      e.target.value = oldContent.substring( 0, selectionStartPos ) + "\t" + oldContent.substring( selectionEndPos );
+
+      // Set the new cursor position (current position + 1 to account for the new tab character).
+      e.target.selectionStart = e.target.selectionEnd = selectionStartPos + 1;
+
+      // Prevent the default action (tabbing to the next field or control).
+      e.preventDefault();
+      
+      this.handleFieldChange(e);
+    }
   }
 }
 
